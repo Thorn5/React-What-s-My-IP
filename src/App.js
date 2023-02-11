3d6d37a4097ac877ef9ac2b056e9f55fedc38e4e
Trygve Horn <trygvehorn@gmail.com>
2023-02-11 10:57:15 +0100

    ss Co-authored-by: Alessandra Silva <alessandra.apds@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import TestAPI from './Components/TestAPI';
+
 
 function App() {
   return (
     <div className="App">
     <h3>SandBox Branch</h3>
+    <TestAPI />
     </div>
   );
 }
--- src/Components/TestAPI.js
+++ src/Components/TestAPI.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const TestAPI = () => {
- 
-    conconst [error, setError] = useState(null);
-    st const [loading, setLoading] = useState(true);[data, setData] = useState({});
-    
-    
-     return (
-    <div>Test
-        
-  
-        const fetchData = () => {      
-  fetch("htt.then((response) => response.json())
-        ps:/.then((data) => setData(data))
-        /geo.catch(() => setError("error message") )
-        
-  .finally(() => setLoading(true))  }fy.org/api/v2/country,city,vpn?apiKey=at_TVG63iHfwfaDK3aS3cXHeIiGHgrbO&ipAddress=8.8.8.8")
-          v>
-  )
-}
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-export default TestAPI
+    const fetchData = () => {
+        fetch(
+            "https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_TVG63iHfwfaDK3aS3cXHeIiGHgrbO&ipAddress=8.8.8.8"
+        )
+            .then((response) => response.json())
+            .then((data) => setData(data))
+            .catch(() => setError("error message"))
+            .finally(() => setLoading(true));
+    };
+
+    return (
+        <div>TestAPI.js</div>    
+        );
+};
+
+export default TestAPI;
