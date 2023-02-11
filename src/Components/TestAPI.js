8e49cdbafa9871d89d204de74c16d51c6f0e3291
Alessandra Silva <alessandra.apds@gmail.com>
2023-02-11 09:45:28 +0000

    initiate api

--- src/Components/TestAPI.js
+++ src/Components/TestAPI.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TestAPI = () => {
-  return (
-    <div>TestAPI</div>
+ 
+    conconst [error, setError] = useState(null);
+    st const [loading, setLoading] = useState(true);[data, setData] = useState({});
+    
+    
+     return (
+    <div>Test
+        
+  
+        const fetchData = () => {      
+  fetch("htt.then((response) => response.json())
+        ps:/.then((data) => setData(data))
+        /geo.catch(() => setError("error message") )
+        
+  .finally(() => setLoading(true))  }fy.org/api/v2/country,city,vpn?apiKey=at_TVG63iHfwfaDK3aS3cXHeIiGHgrbO&ipAddress=8.8.8.8")
+          v>
   )
 }
 
