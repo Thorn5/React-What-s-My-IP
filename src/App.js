75312afb56b8984eab55094580a868b49c398bfc
Trygve Horn <trygvehorn@gmail.com>
2023-02-11 17:08:44 +0100

    sd

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import GetApiData from "./Components/GetApiData";
 
 function App() {
   const [apiData, setApiData] = useState(null);
-  const [apiKey, setApiKey] = useState(REACT_APP_IPIFY_API);
+  const [apiKey, setApiKey] = useState(develoment.env.REACT_APP_IPIFY_API);
   const [apiString, setApiString] = useState(
     `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`
   );
