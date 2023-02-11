0f81f2124ae2647eb4d455804a9e394901970541
Trygve Horn <trygvehorn@gmail.com>
2023-02-11 12:37:41 +0100

    sd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import React, { useState } from "react";
 import TestAPI from './Components/TestAPI';
 
+
 function App() {
+  const [apiData, setApiData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [userUrl, setUserUrl] = useState("8.8.8.8");
   return (
     <div className="App">
     <h3>SandBox Branch</h3>
-    <TestAPI />
+    <TestAPI apiData={apiData} setApiData={setApiData} error={error} setError={setError} loading={loading} setLoading={setLoading} userUrl={userUrl} setUserUrl={setUserUrl}/>
     </div>
   );
 }
 
 export default App;
+
+
+
--- src/Components/TestAPI.js
+++ src/Components/TestAPI.js
@@ -1,56 +1,53 @@
 import React, { useState, useEffect } from "react";
 
-const TestAPI = () => {
-    const [apiData, setApiData] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [userUrl, setUserUrl] = useState("87.153.54.148")
+const TestAPI = ({apiData, setApiData, error, setError, loading, setLoading, userUrl, setUserUrl
+}) => {
 
-    const ale_key = "at_TVG63iHfwfaDK3aS3cXHeIiGHgrbO"
 
-    useEffect( () => {
+    const ale_key = "at_TVG63iHfwfaDK3aS3cXHeIiGHgrbO";
 
-        async function fetchData () {
-            // await  fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${ale_key}&ipAddress=${userUrl}`
-            await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_ZMa23mQ2nIaxv2YKfDSuCwZUPeQ5w&ipAddress=${userUrl}`
+    useEffect(() => {
+        async function fetchData() {
+            // fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_ZMa23mQ2nIaxv2YKfDSuCwZUPeQ5w&ipAddress=${userUrl}`
+            await fetch(
+                `https://geo.ipify.org/api/v2/country,city?apiKey=at_ZMa23mQ2nIaxv2YKfDSuCwZUPeQ5w&ipAddress=${userUrl}`
             )
                 .then((response) => response.json())
                 .then((data) => setApiData(data))
                 .catch(() => setError("error message"))
                 .finally(() => setLoading(true));
+        }
+        fetchData();
+    }, []);
 
-            }
-            // console.log(apiData);
-            // console.log(apiData.ip);
-            fetchData();
-        }, []);
-            
+    const GoogleClick = () => {
+        setUserUrl("8.8.8.8");
+    };
 
-const TrygClick = () => {
-    setUserUrl("87.153.54.148")
+    const TrygClick = () => {
+        setUserUrl("87.153.54.148");
+    };
+
+    const AlleClick = () => {
+        setUserUrl("93.229.201.99");
+    };
+
+    const JapanClick = () => {
+        setUserUrl("101.110.0.0");
+    };
+
     console.log(userUrl);
-}
-
-const AlleClick = () => {
-    setUserUrl("93.229.201.99")
-    console.log(userUrl);
-}
-
-const JapanClick =()=>{
-    setUserUrl("101.110.0.0")
-}
-
-
-return (
-    <>
-        <button onClick={TrygClick}>Tryg</button>
-        <button onClick={AlleClick}>Alle</button>
-        <button onClick={JapanClick}>Japan</button>
-        <div>Your IP is: {apiData.ip}</div>
-        <div>Your location is: {apiData.location.city}</div>
-        {/* <div>Your location is: {apiData.isp}</div> */}
-    </>
-);
+    return (
+        <div>
+            <button onClick={TrygClick}>Tryg</button>
+            <button onClick={AlleClick}>Alle</button>
+            <button onClick={JapanClick}>Japan</button>
+            <button onClick={GoogleClick}>Google</button>
+            {/* <div>Your IP is: {apiData.ip}</div>
+            <div>Your location is: {apiData.location.city}</div>
+            <div>Your location is: {apiData.isp}</div> */}
+        </div>
+    );
 };
 
 export default TestAPI;
