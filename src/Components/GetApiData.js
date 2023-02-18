be96c3c13264102e4d8be61537c96aa0e9ba7d39
Trygve Horn <trygvehorn@gmail.com>
2023-02-18 16:51:03 +0100

    ss

--- src/Components/GetApiData.js
+++ src/Components/GetApiData.js
@@ -1,5 +1,8 @@
-import { Card, Grid } from "@mui/material";
-import { Container } from "@mui/system";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
 import React, { useState, useEffect } from "react";
 import DisplayIPDetails from "../Components/DisplayIPDetails";
 import ShowLocation from "../Components/ShowLocation";
@@ -27,8 +30,13 @@ const GetApiData = ({ apiData, setApiData }) => {
     setLoading(false)
   }, [])
 
-
-  console.log(apiData);
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
   return (
     <div>
@@ -36,48 +44,98 @@ const GetApiData = ({ apiData, setApiData }) => {
         <p>Loading..</p>
       ) : apiData ? (
         <>
-          <Grid container spacing={2}
-            style={{
-              // center all the elements
-              display: "flex",
-              alignItems: "center",
-            }}>
-            {apiData.country}
-            <Grid item xs={6}
-              style={{
-                // center all the elements
-                // display: "flex",
-                alignContent: "center",
-              }}>
-              <Card variant="outlined"
-                sx={{
-                  display: 'inline-block',
-                  mx: '2px',
-                  transform: 'scale(1)'
-                }} >
-                <DisplayIPDetails apiData={apiData} />
-              </Card>
-              <Card variant="outlined"
-                sx={{
-                  display: 'inline-block',
-                  mx: '2px',
-                  transform: 'scale(1)'
-                }} >
-                <DateInfo apiData={apiData} />
-              </Card>
-              <Card variant="outlined"
-                sx={{
-                  display: 'inline-block',
-                  mx: '2px',
-                  transform: 'scale(1)'
-                }} >
-                <CountryInfo apiData={apiData} />
-              </Card>
+          <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid xs={12} md={5} lg={4}>
+          <Item>Email subscribe section</Item>
+        </Grid>
+        <Grid container xs={12} md={7} lg={8} spacing={4}>
+          <Grid xs={6} lg={3}>
+            <Item>
+              <Box
+                id="category-a"
+                sx={{ fontSize: '12px', textTransform: 'uppercase' }}
+              >
+                Category A
+              </Box>
+              <Box component="ul" aria-labelledby="category-a" sx={{ pl: 2 }}>
+                <li>Link 1.1</li>
+                <li>Link 1.2</li>
+                <li>Link 1.3</li>
+              </Box>
+            </Item>
+          </Grid>
+          <Grid xs={6} lg={3}>
+            <Item>
+              <Box
+                id="category-b"
+                sx={{ fontSize: '12px', textTransform: 'uppercase' }}
+              >
+                Category B
+              </Box>
+              <Box component="ul" aria-labelledby="category-b" sx={{ pl: 2 }}>
+                <li>Link 2.1</li>
+                <li>Link 2.2</li>
+                <li>Link 2.3</li>
+              </Box>
+            </Item>
+          </Grid>
+          <Grid xs={6} lg={3}>
+            <Item>
+              <Box
+                id="category-c"
+                sx={{ fontSize: '12px', textTransform: 'uppercase' }}
+              >
+                Category C
+              </Box>
+              <Box component="ul" aria-labelledby="category-c" sx={{ pl: 2 }}>
+                <li>Link 3.1</li>
+                <li>Link 3.2</li>
+                <li>Link 3.3</li>
+              </Box>
+            </Item>
+          </Grid>
+          <Grid xs={6} lg={3}>
+            <Item>
+              <Box
+                id="category-d"
+                sx={{ fontSize: '12px', textTransform: 'uppercase' }}
+              >
+                Category D
+              </Box>
+              <Box component="ul" aria-labelledby="category-d" sx={{ pl: 2 }}>
+                <li>Link 4.1</li>
+                <li>Link 4.2</li>
+                <li>Link 4.3</li>
+              </Box>
+            </Item>
+          </Grid>
+        </Grid>
+        <Grid
+          xs={12}
+          container
+          justifyContent="space-between"
+          alignItems="center"
+          flexDirection={{ xs: 'column', sm: 'row' }}
+          sx={{ fontSize: '12px' }}
+        >
+          <Grid sx={{ order: { xs: 2, sm: 1 } }}>
+            <Item>© Copyright</Item>
+          </Grid>
+          <Grid container columnSpacing={1} sx={{ order: { xs: 1, sm: 2 } }}>
+            <Grid>
+              <Item>Link A</Item>
             </Grid>
-            <Grid item xs={2}>
-              <ShowLocation apiData={apiData} />
+            <Grid>
+              <Item>Link B</Item>
+            </Grid>
+            <Grid>
+              <Item>Link C</Item>
             </Grid>
           </Grid>
+        </Grid>
+      </Grid>
+    </Box>
         </>
       ) : (
         <p>{error}</p>
