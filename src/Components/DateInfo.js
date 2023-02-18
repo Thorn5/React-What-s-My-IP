37b8f57d785528a1d36757863aa23e2886f4f964
Alessandra Silva <alessandra.apds@gmail.com>
2023-02-18 15:34:27 +0100

    git

--- src/Components/DateInfo.js
+++ src/Components/DateInfo.js
@@ -2,21 +2,10 @@ import React from "react";
 import { DateTime } from "luxon";
 
 console.log(DateTime.now(), "test time");
-const localDate = DateTime.local({
-  year: 2017,
-  month: 5,
-  day: 15,
-  hour: 17,
-  minute: 36,
-});
+const localDate = DateTime.local();
 console.log(DateTime.local({ zone: "America/New_York" }), "test 2");
 const nyDate = DateTime.local({
   zone: "America/New_York",
-  year: 2017,
-  month: 5,
-  day: 15,
-  hour: 17,
-  minute: 36,
 });
 
 const DateInfo = () => {
