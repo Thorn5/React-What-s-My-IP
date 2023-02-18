69aed770e550f947cd7dd88d2f5637fe2253ffac
JoanClaverol <41630699+JoanClaverol@users.noreply.github.com>
2023-02-18 14:41:34 +0100

    returned to the orginial state of the app

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <GetApiData
+      <GetApiData
         apiData={apiData}
         setApiData={setApiData}
-      /> */}
-      <Team />
+      />
+      {/* <Team /> */}
     </div>
   );
 }
