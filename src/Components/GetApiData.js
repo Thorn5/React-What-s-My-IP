c59a9650fc5feecd8566dcd08f010ade15919b0e
Alessandra Silva <alessandra.apds@gmail.com>
2023-02-18 12:52:55 +0100

    date informtion

--- src/Components/GetApiData.js
+++ src/Components/GetApiData.js
@@ -8,6 +8,7 @@ const GetApiData = ({ apiData, setApiData }) => {
   const [error, setError] = useState(null);
   // const apiUrl = (`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_IPIFY_API}`)
   // const apiUrl = (`http://ip-api.com/json/`)
+
   const apiUrl = `http://ip-api.com/json/`;
   useEffect(() => {
     function fetchData() {
@@ -26,7 +27,7 @@ const GetApiData = ({ apiData, setApiData }) => {
         <p>Loading..</p>
       ) : apiData ? (
         <>
-        {apiData.country}
+          {apiData.country}
           <DisplayIPDetails apiData={apiData} />
           <ShowLocation apiData={apiData} />
           <CountryInfo apiData={apiData} />
